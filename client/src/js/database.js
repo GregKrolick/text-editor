606fbe89a5273ece00c17d084039194ed872f0ed
Greg Krolick <gregrobk@live.com>
2022-12-01 22:45:41 -0500

    add get database logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -23,6 +23,13 @@ console.log("Updated text entry has been saved to the database!", result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result.value);
+  return result.value;
+  };
 initdb();
