57e2def6484201f27678b267f4acc15972c1acc3
Greg Krolick <gregrobk@live.com>
2022-12-01 23:04:43 -0500

    add comments to database.js and install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,13 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// event handler for the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   window.defferedPrompt = event;
   butInstall.classList.toggle('hidden', false);
 });
   
-// TODO: Implement a click event handler on the `butInstall` element
+// click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
   const promptEvent = window.defferedPrompt;
 
@@ -18,7 +18,7 @@ butInstall.addEventListener('click', async () => {
   window.defferedPrompt = null;
   butInstall.classList.toggle('hidden', true);});
 
-// TODO: Add an handler for the `appinstalled` event
+// handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   window.defferedPrompt = null;
 });
